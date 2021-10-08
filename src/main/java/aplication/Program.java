3a71e7254fb0e907dd47dfe16405e01826c29889
victor moraes <victorhardflipmoraes@outlook.com>
2021-10-08 17:28:05 -0400

    inserindo dados no banco

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -1,15 +1,30 @@
 package aplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Program {
 	public static void main(String[]args) {
-		Pessoa p1 = new Pessoa(1,"victor","vcmboard5@gmail.com");
-		Pessoa p2 = new Pessoa(2,"igor","vcigor@gmail.com");
-		Pessoa p3 = new Pessoa(3,"natalia","nat@gmail.com");
+		Pessoa p1 = new Pessoa("victor","vcmboard5@gmail.com");
+		Pessoa p2 = new Pessoa("igor","vcigor@gmail.com");
+		Pessoa p3 = new Pessoa("natalia","nat@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();	
+		
+		//ABRE A TRANSAÇÃO POSSIBILITANDO AS MANIPULAÇÕES NO BANCO
+		em.getTransaction().begin();
+		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		//CONFIRMA AS ALTERAÇÕES REALIZADAS
+		em.getTransaction().commit();
+		System.out.println("pronto!");
+		
+		em.close();
 	}
 }	
--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -2,25 +2,37 @@ package dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable{
 	private static final long serialVersionUID= 1l;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+
+	/*RENOMEA O NOME DA COLUNA NO BANCO DE DADOS*/
+	@Column(name="nome")
 	private String name;
+	
 	private String email;
 	
 	public Pessoa() {
 		
 	}
 
-
-	public Pessoa(Integer id,String name, String email) {
+	public Pessoa(String name, String email) {
 		super();
-		this.id = id;
 		this.name = name;
 		this.email = email;
-		
 	}
+	
+	
 
 
 	public int getId() {
